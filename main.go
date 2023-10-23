a3a16a86c91823c29ab28c71ea00620499e8be11
sdbvtht <63416139+sdbvtht@users.noreply.github.com>
2023-10-23 21:19:09 +0800

    Update main.go

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 	//                          vvvvvvvvvvvvvv                          //
 	//                               vvvv                               //
 
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/antiabuse" // 违禁词
+	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/antiabuse" // 违禁词
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chat" // 基础词库
 
